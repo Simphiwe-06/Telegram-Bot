a4f04541eef713b62471fb0f597f2454d6d5cae1
Simphiwe Thokozani Mashinini <s224136879@mandela.ac.za>
2024-08-19 14:29:19 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import spacy
-import python-telegram-bot
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
